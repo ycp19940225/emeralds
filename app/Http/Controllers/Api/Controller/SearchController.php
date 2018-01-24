2a071af4a80a6c0e793c2633017b957fc490814c
ycp0225 <820363773@qq.com>
2018-01-24 23:33:45 +0800

    update api

--- app/Http/Controllers/Api/Controller/SearchController.php
+++ app/Http/Controllers/Api/Controller/SearchController.php
@@ -49,7 +49,90 @@ class SearchController extends BaseController
        $res = DB::table('emerald_cat as a')
            ->leftJoin('emerald_goods as b','a.id','=','b.cat_id')
            ->where('a.cat_name',$cat)
+           ->where('a.deleted_at',0)
+           ->where('a.status',1)
+           ->where('a.checked',1)
            ->get();
         return API_MSG($res,'获取该分类商品成功！');
+    }
+
+    /**
+     * 通过分类获取商品
+     *
+     *
+     * @Post("http://temp.cqquality.com/api/goods/search/type")
+     * @Parameters({
+     *     @Parameter("cat_id", type="varchar", required=true, description="一级品种ID"),
+     *     @Parameter("type_id", type="varchar", required=true, description="二级分类ID"),
+     *     @Parameter("type_value", type="varchar", required=true, description="三级级明细")
+     * }),
+     * @Transaction({
+     *      @Request({
+    "cat_id":"114",
+    "type_id":"137",
+    "type_value":"飘绿"
+    })}),
+     *@Transaction({
+     *      @Response(200, body={
+    "status": "true",
+    "code": "200",
+    "msg": "获取该分类商品成功！"
+
+    }),
+     *      @Response(500, body={"error": {
+    "status": "false",
+    "code": 500,
+    "msg": "获取失败！",
+    "data": ""
+     *     }})
+     * })
+     */
+    public function getGoodsByCat(Request $request)
+    {
+        $cat_id = $request->input('cat_id');
+        $type_id = $request->input('type_id');
+        $type_value = $request->input('type_value');
+        $res = DB::table('emerald_goods as a')
+            ->leftJoin('emerald_goods_type as b','a.id','=','b.goods_id')
+            ->where('a.cat_id',$cat_id)
+            ->where('b.type_id',$type_id)
+            ->where('b.type_val',$type_value)
+            ->get();
+        return API_MSG($res,'获取详细分类商品成功！');
+    }
+    /**
+     * 通过筛选获取商品
+     *
+     *
+     * @Post("http://temp.cqquality.com/api/goods/search/type")
+     * @Parameters({
+     *     @Parameter("cat_id", type="varchar", required=true, description="一级品种ID"),
+     *     @Parameter("type_id", type="varchar", required=true, description="二级分类ID"),
+     *     @Parameter("type_value", type="varchar", required=true, description="三级级明细")
+     * }),
+     * @Transaction({
+     *      @Request({
+    "cat_id":"114",
+    "type_id":"137",
+    "type_value":"飘绿"
+    })}),
+     *@Transaction({
+     *      @Response(200, body={
+    "status": "true",
+    "code": "200",
+    "msg": "获取该分类商品成功！"
+
+    }),
+     *      @Response(500, body={"error": {
+    "status": "false",
+    "code": 500,
+    "msg": "获取失败！",
+    "data": ""
+     *     }})
+     * })
+     */
+    public function getGoodsByFields()
+    {
+        
     }
 }
