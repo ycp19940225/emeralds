97ebd920a770758247d1a5cdc897724b2167be66
820363773@qq.com <820363773@qq.com>
2018-02-09 15:58:59 +0800

    update API

--- app/Models/Admin/Agent.php
+++ app/Models/Admin/Agent.php
@@ -21,7 +21,10 @@ class Agent extends Base
      * 可以被集体赋值的表字段
      * @var array
      */
-    public $fillable = array('id','agent_code','agent_name','telphone','booth_name','pm','wx','bank_name','bank_type','bank_code','alipay_code','agent_pic','status','license_number','updated_at','deleted_at','user_id');
+    public $fillable = array('id','agent_code','agent_name','telphone','booth_name','pm','wx','bank_name','bank_type','bank_code',
+        'bank_name_back','bank_type_back','bank_code_back','alipay_code','alipay_name',
+        'alipay_code_back','alipay_name_back','card_front','card_back',
+        'agent_pic','status','license_number','updated_at','deleted_at','user_id');
 
     public function add($data)
     {
