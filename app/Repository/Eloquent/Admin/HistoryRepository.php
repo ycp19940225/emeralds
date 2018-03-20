cb02bc97e25bf80aca5214c504214b6f3bac14c7
ycp0225 <820363773@qq.com>
2018-03-20 19:45:33 +0800

    update collect

--- app/Repository/Eloquent/Admin/HistoryRepository.php
+++ app/Repository/Eloquent/Admin/HistoryRepository.php
@@ -29,18 +29,23 @@ class HistoryRepository
 
     public function getAll($id,$type)
     {
+        $data =$this->collectModel
+            ->where('deleted_at',0)
+            ->where('user_id',$id)
+            ->with('articles')
+            ->with('goods')
+            ->get();
+        foreach ($data as $k=>$v){
+            if($v->articles !== null){
+                $res['articles'][$k] = $v->articles;
+            }else{
+                $res['goods'][$k] = $v->goods;
+            }
+        }
         if($type == 1){
-            return $this->collectModel
-                ->where('deleted_at',0)
-                ->where('user_id',$id)
-                ->with('articles')
-                ->get();
+            return $res['articles'];
         }else if($type == 2){
-            return $this->collectModel
-                ->where('deleted_at',0)
-                ->where('user_id',$id)
-                ->with('goods')
-                ->get();
+            return $res['goods'];
         }
     }
 
