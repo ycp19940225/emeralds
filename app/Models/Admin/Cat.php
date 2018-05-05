2d7b4ba4bcee3d9391f246ebca23be98c21fb433
ycp0225 <820363773@qq.com>
2018-05-05 17:52:48 +0800

    update

--- app/Models/Admin/Cat.php
+++ app/Models/Admin/Cat.php
@@ -52,7 +52,7 @@ class Cat extends Base
 
     public function getAll()
     {
-        return $this->where('deleted_at',0)->orderBy('sort')->get();
+        return $this->where('deleted_at',0)->orderBy('sort','desc')->get();
     }
 
     public function find($id)
