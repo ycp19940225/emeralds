7441f1da5f8dff1ff418f19d5d42d08e55e549b0
ycp0225 <820363773@qq.com>
2018-05-06 18:05:02 +0800

    update

--- app/Http/Controllers/Api/Controller/ChatController.php
+++ app/Http/Controllers/Api/Controller/ChatController.php
@@ -210,7 +210,7 @@ class ChatController extends BaseController
     public function getChat(Request $request){
         $touid= $request->input('touid');
         $User = DB::table('emerald_chat'); // 实例化User对象
-        $list = $User->select('id','touid','uid','content','state')->where('uid',$touid)->groupBy('touid')->orderBy('created_at','desc')->get();
+        $list = $User->select('id','touid','uid','content','state')->where('touid',$touid)->groupBy('uid')->orderBy('created_at','desc')->get();
         $list = $list->toArray();
         if(!empty($list)){
             foreach ($list as $k=>$v){
@@ -224,7 +224,7 @@ class ChatController extends BaseController
                 }
                 $list[$k]->name = isset($name->nickname) ? $name->nickname:"";
             }
-        }elseif(empty($list)){
+        }/*elseif(empty($list)){
             $list = DB::table('emerald_chat')->select('id','touid','uid','content','state')->where('touid',$touid)->groupBy('uid')->orderBy('created_at','desc')->get();
             foreach ($list as $k=>$v){
                 $is_myimg['id']=$v->uid;
@@ -241,7 +241,7 @@ class ChatController extends BaseController
                 }
                 $list[$k]->name = isset($name->nickname) ? $name->nickname:"";
             }
-        }
+        }*/
         if(!empty($list)){
             return $list;
         }else{
