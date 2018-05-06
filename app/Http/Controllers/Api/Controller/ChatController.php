bbe4f4d6eab2a6cda1fa890666f1a6f0fd94db8a
ycp0225 <820363773@qq.com>
2018-05-06 17:01:22 +0800

    update

--- app/Http/Controllers/Api/Controller/ChatController.php
+++ app/Http/Controllers/Api/Controller/ChatController.php
@@ -226,6 +226,7 @@ class ChatController extends BaseController
                 }
                 $list[$k]->name = isset($name->nickname) ? $name->nickname:"";
             }
+            dd(1);
         }elseif(empty($list)){
             $list = $User->select('id','touid','uid','content','state')->where('touid',$touid)->groupBy('uid')->orderBy('created_at','desc')->get();
             foreach ($list as $k=>$v){
