57198a59dfe9138c8bc71e4586bd794116e9b7d0
ycp0225 <820363773@qq.com>
2018-05-06 17:02:03 +0800

    update

--- app/Http/Controllers/Api/Controller/ChatController.php
+++ app/Http/Controllers/Api/Controller/ChatController.php
@@ -226,8 +226,8 @@ class ChatController extends BaseController
                 }
                 $list[$k]->name = isset($name->nickname) ? $name->nickname:"";
             }
-            dd(1);
         }elseif(empty($list)){
+            dd(1);
             $list = $User->select('id','touid','uid','content','state')->where('touid',$touid)->groupBy('uid')->orderBy('created_at','desc')->get();
             foreach ($list as $k=>$v){
                 $is_myimg['id']=$v->uid;
